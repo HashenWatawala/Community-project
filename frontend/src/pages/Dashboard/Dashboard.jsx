58297f226e1ac9f456a5f509f55d16e628d21990
concepts0046 <sandharusankalpa32@gmail.com>
2025-10-18 16:01:58 +0530

    created the dashboard compoenent and test it with the dashboard

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import Sidebar from "../../components/Sidebar";
 
 const Dashboard = () => {
   return (
-    <div>
-      Dashboard
+    <div className="flex">
+      <Sidebar />
+      <div className="flex-1 p-10">
+        <h1 className="text-3xl font-semibold text-gray-800">Dashboard</h1>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
